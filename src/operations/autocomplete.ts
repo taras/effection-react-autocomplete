6c68dcf34e1ace2b0f40df4691dd1a626280e082
Taras Mankovski <taras@frontside.com>
2023-12-29 09:48:57 -0500

    Updated with charles' example

--- src/operations/autocomplete.ts
+++ src/operations/autocomplete.ts
@@ -7,30 +7,39 @@ import {
   resource,
   sleep,
   spawn,
+  suspend,
 } from "effection";
 import { Operation } from "effection";
 import { Stream } from "effection";
 
-export type AutocompleteResult<TResult> = [Stream<TResult, never>, (value: string) => void];
+export type AutocompleteResult<TResult> = [
+  Stream<TResult, never>,
+  (value: string) => void
+];
 
 export function useAutocompleteOperation<TResult>(
-  getResult: (value: string) => Operation<TResult>,
+  getResult: (value: string) => Operation<TResult>
 ): Operation<AutocompleteResult<TResult>> {
   return resource(function* (provide) {
     const values = createSignal<string>();
     const results = createChannel<TResult, never>();
 
     yield* spawn(function* () {
+      let current = yield* spawn(() => suspend()); // start with a placeholder
+
       for (const value of yield* each(values)) {
-        console.log("getting values", value)
-        yield* race<string | void>([
-          first(values),
-          call(function* (): Operation<void> {
-            yield* sleep(250);
-            const result = yield* getResult(value);
-            yield* results.send(result);
-          }),
-        ]);
+        // stop the current task;
+        yield* current.halt();
+
+        console.log("running loop")
+
+        current = yield* spawn(function* () {
+          yield* sleep(250);
+
+          const result = yield* getResult(value);
+
+          yield* results.send(result);
+        });
         yield* each.next();
       }
     });
@@ -38,9 +47,3 @@ export function useAutocompleteOperation<TResult>(
     yield* provide([results, values.send]);
   });
 }
-
-const first = <T>(stream: Stream<T, never>) => call(function* () {
-  const subscription = yield* stream;
-  const next = yield* subscription.next();
-  return next.value;
-});
